fe54184e655fd37b3b946ef1b5d94f89ee96c1a4
prinxard <prinxard@gmail.com>
2020-09-22 08:39:30 +0100

    added dropdown to navbar and pushed category table to Db

---
+++ SteekiT.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SteekiT.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Category Name")]
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
