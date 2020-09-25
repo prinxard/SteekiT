0b943e63eaa4e82077271359d4969867e5f3c61f
prinxard <prinxard@gmail.com>
2020-09-25 16:24:02 +0100

    Implemented Repository interface

---
+++ SteekiT.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using SteekiT.DataAccess.Data;
+using SteekiT.DataAccess.Repository.IRepository;
+using SteekiT.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteekiT.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ SteekiT.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteekiT.DataAccess.Repository.IRepository
+{
+    interface ICategoryRepository
+    {
+    }
+}
