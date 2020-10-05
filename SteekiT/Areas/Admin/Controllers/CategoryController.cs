0e59e261d3d1664e0f950679f61a92517dc94e3d
prinxard <prinxard@gmail.com>
2020-10-05 16:07:31 +0100

    added categories controller

---
+++ SteekiT/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SteekiT.DataAccess.Repository;
+using SteekiT.DataAccess.Repository.IRepository;
+
+namespace SteekiT.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+       
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
