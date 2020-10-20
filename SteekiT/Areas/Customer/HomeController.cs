96a1719ee3183de93e2a744c8616d76976f80cb4
Prinxard <prinxard@gmail.com>
2020-10-20 16:50:10 +0100

    Displayed Products to HomePage

--- SteekiT/Areas/Customer/HomeController.cs
+++ SteekiT/Areas/Customer/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SteekiT.DataAccess.Repository.IRepository;
+using SteekiT.Models;
 using SteekiT.Models.ViewModels;
 
 namespace SteekiT.Areas.Customers.Controllers
@@ -13,15 +15,18 @@ namespace SteekiT.Areas.Customers.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
@@ -32,7 +37,7 @@ namespace SteekiT.Areas.Customers.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
