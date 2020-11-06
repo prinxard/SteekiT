58617656a1f7d9f36ea168cf8992c0cc3f556c82
prince unachukwu <prinxard@gmail.com>
2020-11-06 16:32:03 +0100

    scafolded Identity Properties

--- SteekiT/Areas/Admin/Controllers/CategoryController.cs
+++ SteekiT/Areas/Admin/Controllers/CategoryController.cs
@@ -12,7 +12,7 @@ namespace SteekiT.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
-    {
+    {  
        
         private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork unitOfWork)
