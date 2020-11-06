00361b32f7ea70eb897405bfb51a2b1c9c32a1ad
prince unachukwu <prinxard@gmail.com>
2020-11-06 09:13:44 +0100

    added application user to database

---
+++ SteekiT.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SteekiT.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string StreetAdderss { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
